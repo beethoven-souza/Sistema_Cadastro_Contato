b5d19d630276946cc00f0043222494472e903a9b
Beethoven Souza <bthoven2009@hotmail.com>
2022-03-15 15:46:12 -0300

    criandoProjeto/controllerContatos/viewsContato

---
+++ SistemaCadastroContatos/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SistemaCadastroContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaCadastroContatos.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            home.Nome = "Beethoven Souza";
+            home.Email = "bthoven2009@hotmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
